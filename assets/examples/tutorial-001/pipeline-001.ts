b69e03a2a377dc00f683aa73ddec59d302412cb9
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-07-26 16:50:22 +0800

    fix camera clear

--- assets/examples/tutorial-001/pipeline-001.ts
+++ assets/examples/tutorial-001/pipeline-001.ts
@@ -64,24 +64,11 @@ class HelloWorldPipeline implements rendering.PipelineBuilder {
         this._windows.set(camera.window, info);
         return info;
     }
-    private initGameCamera (
-        ppl: rendering.BasicPipeline,
-        camera: renderer.scene.Camera,
-        id: number,
-        width: number,
-        height: number): void {
-        const device = ppl.device;
-        // Main Target
+    private initGameCamera (ppl: rendering.BasicPipeline, camera: renderer.scene.Camera, id: number, width: number, height: number): void {
         ppl.addRenderWindow(`Color${id}`, gfx.Format.BGRA8, width, height, camera.window);
         ppl.addDepthStencil(`DepthStencil${id}`, gfx.Format.DEPTH_STENCIL, width, height);
     }
-    private updateGameCamera (
-        ppl: rendering.BasicPipeline,
-        camera: renderer.scene.Camera,
-        id: number,
-        width: number,
-        height: number): void {
-        // Main Target
+    private updateGameCamera (ppl: rendering.BasicPipeline, camera: renderer.scene.Camera, id: number, width: number, height: number): void {
         ppl.updateRenderWindow(`Color${id}`, camera.window);
         ppl.updateDepthStencil(`DepthStencil${id}`, width, height);
     }
@@ -96,14 +83,38 @@ class HelloWorldPipeline implements rendering.PipelineBuilder {
         if (camera.scene === null) {
             return;
         }
-        const scene = camera.scene;
-        const mainLight = scene.mainLight;
+
+        // Camera
+        this._clearColor.x = camera.clearColor.x;
+        this._clearColor.y = camera.clearColor.y;
+        this._clearColor.z = camera.clearColor.z;
+        this._clearColor.w = camera.clearColor.w;
+
+        // Viewport
+        this._viewport.left = camera.viewport.x * width;
+        this._viewport.top = camera.viewport.y * height;
+        this._viewport.width = camera.viewport.z * width;
+        this._viewport.height = camera.viewport.w * height;
 
         // Forward Lighting
         {
             const pass = ppl.addRenderPass(width, height, 'default');
-            pass.addRenderTarget(`Color${id}`, gfx.LoadOp.CLEAR);
-            pass.addDepthStencil(`DepthStencil${id}`, gfx.LoadOp.CLEAR);
+            pass.setViewport(this._viewport);
+            if (camera.clearFlag & gfx.ClearFlagBit.COLOR) {
+                pass.addRenderTarget(`Color${id}`, gfx.LoadOp.CLEAR, gfx.StoreOp.STORE, this._clearColor);
+            } else {
+                pass.addRenderTarget(`Color${id}`, gfx.LoadOp.LOAD);
+            }
+            if (camera.clearFlag & gfx.ClearFlagBit.DEPTH_STENCIL) {
+                pass.addDepthStencil(
+                    `DepthStencil${id}`,
+                    gfx.LoadOp.CLEAR,
+                    gfx.StoreOp.STORE,
+                    camera.clearDepth,
+                    camera.clearStencil);
+            } else {
+                pass.addDepthStencil(`DepthStencil${id}`, gfx.LoadOp.LOAD);
+            }
             pass.addQueue(rendering.QueueHint.NONE)
                 .addSceneOfCamera(
                     camera,
@@ -117,6 +128,8 @@ class HelloWorldPipeline implements rendering.PipelineBuilder {
         }
     }
     readonly _windows = new Map<any, WindowInfo>();
+    readonly _clearColor = new gfx.Color();
+    readonly _viewport = new gfx.Viewport();
 }
 
 rendering.setCustomPipeline('MyPipeline', new HelloWorldPipeline());
