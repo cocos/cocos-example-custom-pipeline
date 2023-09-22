be8c233967289f61390b8ad3aeaa83a6d0271e05
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-09-22 18:05:34 +0800

    spotlight shadow map added

--- assets/examples/tutorial-003-multi-light/pipeline-003.ts
+++ assets/examples/tutorial-003-multi-light/pipeline-003.ts
@@ -178,11 +178,11 @@ class MultiLightPipeline implements rendering.PipelineBuilder {
 
             // add multi-lights
             // 添加多光源
-            this.lighting.addLightPasses(camera, pass);
+            const pass2 = this.lighting.addLightPasses(id, width, height, camera, ppl, pass);
 
             // add transparent queue
             // 添加透明队列
-            pass.addQueue(rendering.QueueHint.BLEND)
+            pass2.addQueue(rendering.QueueHint.BLEND)
                 .addSceneOfCamera(
                     camera,
                     new rendering.LightInfo(),
