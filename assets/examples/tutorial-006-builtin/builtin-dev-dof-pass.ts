632a5e8d843132d4dff3c93b308c1a5c3adcf748
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-11-15 12:52:53 +0800

    lint

--- assets/examples/tutorial-006-builtin/builtin-dev-dof-pass.ts
+++ assets/examples/tutorial-006-builtin/builtin-dev-dof-pass.ts
@@ -212,10 +212,7 @@ export class BuiltinDevDepthOfFieldPass extends BuiltinDevPipelinePassBuilder
         ppl: rendering.BasicPipeline,
         pplConfigs: Readonly<PipelineConfigs>,
         cameraConfigs: Readonly<CameraConfigs & DofPassConfigs>,
-        window: renderer.RenderWindow,
-        camera: renderer.scene.Camera,
-        nativeWidth: number,
-        nativeHeight: number): void {
+        window: renderer.RenderWindow): void {
         const id = window.renderWindowId;
         if (cameraConfigs.enableDof) {
             ppl.addRenderTarget(`DofRadiance${id}`,
@@ -236,6 +233,7 @@ export class BuiltinDevDepthOfFieldPass extends BuiltinDevPipelinePassBuilder
         }
         --cameraConfigs.remainingPasses;
 
+        assert(!!this._material);
         if (cameraConfigs.remainingPasses === 0) {
             return this._addDepthOfFieldPasses(ppl, pplConfigs,
                 cameraConfigs, this._material,
